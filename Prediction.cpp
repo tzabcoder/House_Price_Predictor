aac83af399973399f7653edbeffcc325b452fb55
tzabcoder <60833046+tzabcoder@users.noreply.github.com>
2020-10-03 14:06:08 -0400

    Update Prediction.cpp

--- Prediction.cpp
+++ Prediction.cpp
@@ -1,19 +1,14 @@
-#ifndef PREDICTION_H
-#define PREDICTION_H
-#include <iostream>
-#include <fstream>
-using namespace std;
+#include "Predictor.h"
 
-class Predictor
-{
-public:
-	//Constructor
-	Predictor();
-	void cost_function();
-	void graph_function();
-	void store_data(int*, int*, int);
-private:
-	ofstream write_file;
-};
+Predictor::Predictor()
+{}
 
-#endif
+void Predictor::store_data(int* price, int* square_feet, int size) {
+	write_file.open("data_file.txt");
+
+	for (int i = 0; i < size; i++) {
+		write_file << square_feet[i] << " " << price[i] << endl;
+	}
+
+	write_file.close();
+}
