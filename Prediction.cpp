1a893b6455c168c5fbb3c4b619a3a93e509fbfae
tzabcoder <60833046+tzabcoder@users.noreply.github.com>
2020-10-03 14:05:26 -0400

    Update Prediction.cpp

--- Prediction.cpp
+++ Prediction.cpp
@@ -1,5 +1,19 @@
-#include "Prediction.h"
+#ifndef PREDICTION_H
+#define PREDICTION_H
+#include <iostream>
+#include <fstream>
+using namespace std;
 
-Prediction::Prediction()
+class Predictor
 {
-}
+public:
+	//Constructor
+	Predictor();
+	void cost_function();
+	void graph_function();
+	void store_data(int*, int*, int);
+private:
+	ofstream write_file;
+};
+
+#endif
